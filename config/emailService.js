5676fb913823c0281f34e0dc8b1f264973f6033f
hoang0650 <48677925+hoang0650@users.noreply.github.com>
2024-09-21 11:22:01 +0700

    update nodemailer

--- config/emailService.js
+++ config/emailService.js
@@ -1,11 +1,23 @@
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const sendEmail = (toEmail, order) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    // service: 'Gmail',
+    // auth: {
+    //   user: 'hoang.ph2158@sinhvien.hoasen.edu.vn',
+    //   pass: 'qzhobjzjkvmoyovv',
+    // },
+    host: 'mail.privateemail.com',
+    port: 465,
+    secure: true,
     auth: {
-      user: 'hoang.ph2158@sinhvien.hoasen.edu.vn',
-      pass: 'qzhobjzjkvmoyovv',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false, // Bỏ qua chứng chỉ tự ký
     },
   });
 
@@ -14,12 +26,12 @@ const sendEmail = (toEmail, order) => {
   }).join('\n');
 
   const mailOptions = {
-    from: 'hoang.ph2158@sinhvien.hoasen.edu.vn',
+    from: 'noreply@yensaothanhbinh.com',
     to: toEmail,
     subject: `Xác nhận đơn hàng ${order.orderId}`,
     text: `Cảm ơn bạn đã đặt hàng!\n\nChi tiết đơn hàng của bạn:\n\nMã đơn hàng: ${order.orderId}\n\nSản phẩm:\n${orderDetails}\n\nTổng tiền: ${formatNumber(order.totalPrice)} VND`,
   };
-  
+
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log('Error sending email:', error);
@@ -30,7 +42,7 @@ const sendEmail = (toEmail, order) => {
 };
 
 function formatNumber(number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
 
 module.exports = sendEmail;
