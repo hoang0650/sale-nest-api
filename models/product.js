2022f5247efae32accfe8530daa260364278aaac
hoang0650 <hoangphan29091999@gmail.com>
2024-10-14 23:32:29 +0700

    add tiktokAffiliateLink

--- models/product.js
+++ models/product.js
@@ -7,6 +7,7 @@ const productSchema = new mongoose.Schema({
     description: String,
     price: { type: Number, required: true },
     discountPrice: Number,
+    tiktokAffiliateLink: String,
     image: [String],
     variants: [{
         size: String,
